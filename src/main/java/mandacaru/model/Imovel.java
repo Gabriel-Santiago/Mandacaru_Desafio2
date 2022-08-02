30df60b597272fc781ac21a53a4c8ebc8440276b
sucoso <65839195+nitsuboy@users.noreply.github.com>
2022-08-02 20:41:33 -0300

    base
    base em spring

--- src/main/java/mandacaru/model/Imovel.java
+++ src/main/java/mandacaru/model/Imovel.java
@@ -1,103 +1,123 @@
-package mandacaru.entities;
+package mandacaru.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-
-@Entity(name = "imovel")
-public class Anuncio {
+public class Imovel {
 	
-	@Id
-	@GeneratedValue
-	@Column(name = "id")
 	private int id;
-	@Column(name = "titulo")
 	private String titulo;
-	@Column(name = "endereco")
 	private String endereco;
-	@Column(name = "metros_quadrados_de_terreno")
 	private int metros_quadrados_de_terreno;
-	@Column(name = "quantidade_de_quartos")
 	private int quantidade_de_quartos;
-	@Column(name = "quantidade_de_banheiros")
 	private int quantidade_de_banheiros;
-	@Column(name = "quantidade_de_vagas_de_garagem")
 	private int quantidade_de_vagas_de_garagem;
-	@Column(name = "preco")
 	private double preco;
-	
-	public Anuncio(int id, String titulo, String endereco, int metros_quadrados_de_terreno, int quantidade_de_quartos,
-			int quantidade_de_banheiros, int quantidade_de_vagas_de_garagem, double preco) {
-		super();
-		this.id = id;
-		this.titulo = titulo;
-		this.endereco = endereco;
-		this.metros_quadrados_de_terreno = metros_quadrados_de_terreno;
-		this.quantidade_de_quartos = quantidade_de_quartos;
-		this.quantidade_de_banheiros = quantidade_de_banheiros;
-		this.quantidade_de_vagas_de_garagem = quantidade_de_vagas_de_garagem;
-		this.preco = preco;
-	}
-	
-	public Anuncio() {
-		super();
-	}
+	private String status;
+	private int dono;
 	
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getEndereco() {
 		return endereco;
 	}
+
 	public void setEndereco(String endereco) {
 		this.endereco = endereco;
 	}
+
 	public int getMetros_quadrados_de_terreno() {
 		return metros_quadrados_de_terreno;
 	}
+
 	public void setMetros_quadrados_de_terreno(int metros_quadrados_de_terreno) {
 		this.metros_quadrados_de_terreno = metros_quadrados_de_terreno;
 	}
+
 	public int getQuantidade_de_quartos() {
 		return quantidade_de_quartos;
 	}
+
 	public void setQuantidade_de_quartos(int quantidade_de_quartos) {
 		this.quantidade_de_quartos = quantidade_de_quartos;
 	}
+
 	public int getQuantidade_de_banheiros() {
 		return quantidade_de_banheiros;
 	}
+
 	public void setQuantidade_de_banheiros(int quantidade_de_banheiros) {
 		this.quantidade_de_banheiros = quantidade_de_banheiros;
 	}
+
 	public int getQuantidade_de_vagas_de_garagem() {
 		return quantidade_de_vagas_de_garagem;
 	}
+
 	public void setQuantidade_de_vagas_de_garagem(int quantidade_de_vagas_de_garagem) {
 		this.quantidade_de_vagas_de_garagem = quantidade_de_vagas_de_garagem;
 	}
+
 	public double getPreco() {
 		return preco;
 	}
+
 	public void setPreco(double preco) {
 		this.preco = preco;
 	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public int getDono() {
+		return dono;
+	}
+
+	public void setDono(int dono) {
+		this.dono = dono;
+	}
+
+	public Imovel(int id, String titulo, String endereco, int metros_quadrados_de_terreno, int quantidade_de_quartos,
+			int quantidade_de_banheiros, int quantidade_de_vagas_de_garagem, double preco, String status, int dono) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.endereco = endereco;
+		this.metros_quadrados_de_terreno = metros_quadrados_de_terreno;
+		this.quantidade_de_quartos = quantidade_de_quartos;
+		this.quantidade_de_banheiros = quantidade_de_banheiros;
+		this.quantidade_de_vagas_de_garagem = quantidade_de_vagas_de_garagem;
+		this.preco = preco;
+		this.status = status;
+		this.dono = dono;
+	}
+
+	public Imovel() {
+		super();
+	}
+
 	@Override
 	public String toString() {
-		return "imovel [id=" + id + ", titulo=" + titulo + ", endereco=" + endereco + ", metros_quadrados_de_terreno="
+		return "Imovel [id=" + id + ", titulo=" + titulo + ", endereco=" + endereco + ", metros_quadrados_de_terreno="
 				+ metros_quadrados_de_terreno + ", quantidade_de_quartos=" + quantidade_de_quartos
 				+ ", quantidade_de_banheiros=" + quantidade_de_banheiros + ", quantidade_de_vagas_de_garagem="
-				+ quantidade_de_vagas_de_garagem + ", preco=" + preco + "]";
+				+ quantidade_de_vagas_de_garagem + ", preco=" + preco + ", status=" + status + ", dono=" + dono + "]";
 	}
 
 }
---
+++ src/main/java/mandacaru/model/Usuario.java
@@ -0,0 +1,95 @@
+package mandacaru.model;
+
+import java.util.Arrays;
+
+public class Usuario {
+	
+	private String nome;
+	private String email;
+	private String senha;
+	private String cpf;
+	private String telefone;
+	private String endereco;
+	private Imovel anuncios[];
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public Imovel[] getAnuncios() {
+		return anuncios;
+	}
+
+	public void setAnuncios(Imovel[] anuncios) {
+		this.anuncios = anuncios;
+	}
+
+	public Usuario(String nome, String email, String senha, String cpf, String telefone, String endereco,
+			Imovel[] anuncios) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		this.senha = senha;
+		this.cpf = cpf;
+		this.telefone = telefone;
+		this.endereco = endereco;
+		this.anuncios = anuncios;
+	}
+
+	public Usuario() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "usuario [nome=" + nome + ", email=" + email + ", senha=" + senha + ", cpf=" + cpf + ", telefone="
+				+ telefone + ", endereco=" + endereco + ", anuncios=" + Arrays.toString(anuncios) + "]";
+	}
+	
+	
+	
+}
